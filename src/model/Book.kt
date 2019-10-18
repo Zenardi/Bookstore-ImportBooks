8d8952aaddb147ad514b7ab19049ec7fb3a5a6a2
Developer <du.zenardi@gmailcom>
2019-10-18 15:59:28 -0300

    first commit

---
+++ src/model/Book.kt
@@ -0,0 +1,48 @@
+package model
+
+public class Book {
+
+    var id: String? = null
+    var title: String? = null
+    var description: String? = null
+    var isbn: String? = null
+    var author: String? = null
+    var genre: String? = null
+    var pages: Int? = null
+    var ageRange: String? = null
+    var price: Float? = null
+    var quantity: Int? = null
+
+
+    constructor() {}
+
+//    constructor(jsonObject: JsonObject) {
+//        this.id = jsonObject.getString("_id")
+//        this.title = jsonObject.getString("title")
+//        this.description = jsonObject.getString("description")
+//        this.isbn = jsonObject.getString("isbn")
+//        this.author = jsonObject.getString("author")
+//        this.genre = jsonObject.getString("genre")
+//        this.pages = jsonObject.getInteger("pages")
+//        this.ageRange = jsonObject.getString("ageRange")
+//        this.price = jsonObject.getFloat("price")
+//        this.quantity = jsonObject.getInteger("quantity")
+//
+//    }
+
+    constructor(book: Book) {
+        this.id = book.id
+        this.title = book.title
+        this.description = book.description
+        this.isbn = book.isbn
+        this.author = book.author
+        this.genre = book.genre
+        this.pages = book.pages
+        this.ageRange = book.ageRange
+        this.price = book.price
+        this.quantity = book.quantity
+
+    }
+
+    constructor(book: Map.Entry<String, Any>)
+}
---
+++ src/util/BookOperations.kt
@@ -0,0 +1,18 @@
+package util
+
+import model.Book
+import java.math.BigInteger
+
+class BookOperations {
+
+fun IsIsbnExistInList(book : ArrayList<Book>, isbn : BigInteger) : Boolean{
+//    var r = book.FindAll(x => x.ISBN == Isbn);
+//
+//    if (r.Count > 0)
+//        return true;
+//    else
+//        return false;
+}
+
+
+}
